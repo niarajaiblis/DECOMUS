ddb95a3a3a5a594f8d22e5d716783f3101c6d433
niarajaiblis <queenastrania33@gmail.com>
2024-12-09 13:19:07 +0700

    Add files via upload

---
+++ Kuliah Projek/activities.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active');
+    } else {
+        sidebar.classList.add('active');
+    }
+}
+
